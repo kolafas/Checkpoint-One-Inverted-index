e38f8a6a580aea47b5cb0c6d2691f94f343cb727
kolafas <aladekolawole15@yahoo.com>
2016-09-22 23:41:01 +0100

    Updated gulpfile.js to automate browsertest

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var browserSync = require("browser-sync");
 var exec = require("child_process").exec;
 
-gulp.task("default", ["serveApp"]);
+gulp.task("default", ["BrowserTest","serveApp"]);
 
 gulp.task('serveApp', function () {
     var f1 = browserSync.create();
@@ -11,12 +11,30 @@ gulp.task('serveApp', function () {
         server : {
             baseDir: "./public"
         },
-        port : 3000,
+        port : 3060,
         ui: {
             port:3020
         }
     });
     //watch for changes of html, css and js in the public folder.
-    gulp.watch("./jasmine/**/*.{html,js,css}").on("change", f1.reload);
+    gulp.watch("./public/**/*.{html,js,css}").on("change", f1.reload);
 });
+
+gulp.task("BrowserTest", function () {
+    var f2 = browserSync.create();
+
+    f2.init({
+        server: {
+          baseDir: ["./public/src/", "./jasmine"],
+          port: 3060,
+          ui: {
+              port: 3050
+          }
+      }
+    });
+
+    gulp.watch('./jasmine/spec/*.js').on("change",f2.reload);
+    gulp.watch("./public/src/*.js").on("change", f2.reload);
+});
+
 
