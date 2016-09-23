7016705073371d06f144b5189b0f4b1911c088ba
kolafas <aladekolawole15@yahoo.com>
2016-09-23 09:10:25 +0100

    Coverall implemented with badge in readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 [![Build Status](https://travis-ci.org/kolafas/Checkpoint-One-Inverted-index.svg?branch=develop)](https://travis-ci.org/kolafas/Checkpoint-One-Inverted-index)
-
+[![Coverage Status](https://coveralls.io/repos/github/kolafas/Checkpoint-One-Inverted-index/badge.svg?branch=master)](https://coveralls.io/github/kolafas/Checkpoint-One-Inverted-index?branch=master)
 # Checkpoint-One-Inverted-index
 Elastic search data indexing example
--- jasmine/karma.conf.js
+++ jasmine/karma.conf.js
@@ -28,13 +28,19 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'public/src/*.js':['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter:{
+      type:"lcov",
+      dir:"coverage/"
+    },
 
 
     // web server port
@@ -45,7 +51,7 @@ module.exports = function(config) {
     colors: true,
 
 
-    // level of logging
+    // level of lo  gging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
@@ -56,7 +62,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
